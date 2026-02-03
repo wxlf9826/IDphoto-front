895dbd8e58010ee7553becad50999e537e4dec7e
wxlf9826 <155796126+wxlf9826@users.noreply.github.com>
2026-02-03 11:32:44 +0800

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,21 @@
+<script>
+	export default {
+		globalData: {
+			url: "http://localhost:8910",
+			appName: "免费证件照"
+		},
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
